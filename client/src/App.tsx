5ac50ce4382316d36d4c4745b1396115754ae789
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2026-04-25 16:34:39 +0500

    protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,26 @@ import { Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
+
 function App() {
 
   return (
     <>
       <section id="center">
-      
-     
+
+
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
 
         </Routes>
-        
+
       </section>
 
     </>
